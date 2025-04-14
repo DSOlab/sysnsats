efd85bf53f6b7c95f41963bc48cb76e066771130
xanthos@durruti <xanthos.papanikolaou@gmail.com>
2025-04-14 19:26:28 +0300

    need test

--- test/unit_tests/test_load_satellite.cpp
+++ test/unit_tests/test_load_satellite.cpp
@@ -1,5 +1,10 @@
 #include "attitude.hpp"
 #include <cstdio>
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#include <cassert>
+#include <random>
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
@@ -19,6 +24,19 @@ int main(int argc, char *argv[]) {
   } catch (std::exception &) {
     fprintf(stderr, "ERROR failed to get measured attitude stream\n");
     return 2;
+  }
+
+  std::uniform_real_distribution<double> unif(-10e0, 20e0);
+  std::default_random_engine re;
+
+  if (sat == dso::SATELLITE::JASON3) {
+    auto t = dso::MjdEpoch(60679, dso::FractionalSeconds(13.123));
+    for (int i=0; i<1000; i++) {
+      dso::FractionalSeconds fsec(unif(re));
+      if (i<10 && fsec<dso::FractionalSeconds(0)) fsec = dso::FractionalSeconds(2e0);
+      t.add_seconds(fsec);
+
+    }
   }
 
   return 0;
